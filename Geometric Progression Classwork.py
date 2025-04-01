9a58c498422528f634fa2e9dd8d5f9aa026dc158
Ozoko-Emmanuel Delight <102426179+freshman198@users.noreply.github.com>
2025-04-01 20:47:27 +0100

    Add files via upload

--- Geometric Progression Classwork.py
+++ Geometric Progression Classwork.py
@@ -1,70 +1,38 @@
 # OZOKO-EMMANUEL DELIGHT
 # 20CJ027488
-# COMPUTER ENGINEERING
-# CEN 524
-
-#Assingment - A python script for the sum of a GP
+## COMPUTER ENGINEERING
+## CEN 524
+#
+# Assingment - A python script for the sum of a GP
 
 import math
 
-def gp_r_less_than_1() -> float: # When r is less than |1|
+print("This program calculates the sum of a Geometric Progression (GP) series.")
 
-    formular = 'a(1-r^n)/(1-r)'
+a = float(input("What is your first number (a)? "))
+r = float(input("What is the common ratio (r)? "))
+n = int(input("What are the number of terms present? "))
 
-    print(f"This is the formula for the GP if r < 1: {formular}")
-    
-    a = int(input("What is your first number(a): "))
-    r = float(input("What is the commin ratio(r): "))
-    if r == 1:
-        raise ValueError("Commom raise cannot be 1")
-    elif r >= 1:
-        raise ValueError("Value of 'r' should be less than 1")
-        
-    n = int(input("How many number of terms(n): "))
-    
-    result = a*(1-math.pow(r,n))/(1-r)
-    
-    print(f"The GP is = {result}")
+def sum_of_gp(a, r, n):
+    if n > 1000000:
+        print("The value of 'n' you input is too large so we take is as infinity")
+        if abs(r) < 1:   
+            result = a/(1-r)
+        else:
+            return None
 
-def gp_r_greater_than_1() -> float:
-
-    formular = 'a((r^1)-1)/(r-1)'
-
-    print(f"This is the formula for the GP if r > 1: {formular}")
-    
-    a = int(input("What is your first number(a): "))
-    r = float(input("What is the commin ratio(r): "))
-    if r == 1:
-        raise ValueError("Commom raise cannot be 1")
-    elif r <= 1:
-        raise ValueError("Value of 'r' should be greater than 1")
-        
-    n = int(input("How many number of terms(n): "))
-    
-    result = a*(math.pow(r,n)-1)/(r-1)
-    
-    print(f"The GP is = {result}")
-
-def gp_n_is_inf() -> float:
-
-    formular = 'a/(1-r)'
-    
-    print(f"This is the formula for the GP if n is infinity: {formular}")
-    a = int(input("What is your first number(a): "))
-    r = float(input("What is the commin ratio(r): "))
-    n = int(input("How many number of terms(n): "))
-    if n > 10000:
-        n = math.inf
     else:
-        raise ValueError("The 'n' value is not big enough to be considered as infinity!!\nTry a number above 10000")
-    if abs(r) >= 1:
-        raise ValueError("Common ratio must be less than 1 in this equation")
-    
-    print("The value 'n' you input is too large so we take is as infinity")
+        if abs(r) > 1:
+            result = a*(math.pow(r, n)-1)/(r-1)
+        elif abs(r) < 1:
+            result = a*(1-math.pow(r, n))/(1-r)
 
-    result = a/(1-r)
-    print(f"The GP = {result}")
+    return result
 
-gp_r_less_than_1()
-gp_r_greater_than_1()
-gp_n_is_inf()
+sum_of_gp = sum_of_gp(a, r, n)
+
+if sum_of_gp is not None:
+    print(f"The sum of the GP is: {sum_of_gp}")
+else:
+    raise ValueError("The common ratio must be less than 1 in this equation")
+
